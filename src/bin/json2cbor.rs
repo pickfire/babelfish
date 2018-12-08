7187bd3b2d3c5c1f307f0b32e0630aba9a39c8ff
Ivan Tham <pickfire@riseup.net>
2018-12-08 16:04:36 +0800

    feat: improve error message

--- src/bin/json2cbor.rs
+++ src/bin/json2cbor.rs
@@ -1,11 +1,12 @@
 use std::io::{self, BufReader, BufWriter};
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let reader = BufReader::new(io::stdin());
     let writer = BufWriter::new(io::stdout());
 
     let mut deserializer = serde_json::Deserializer::from_reader(reader);
     let mut serializer = serde_cbor::Serializer::new(writer);
 
-    serde_transcode::transcode(&mut deserializer, &mut serializer).unwrap();
+    serde_transcode::transcode(&mut deserializer, &mut serializer)?;
+    Ok(())
 }
