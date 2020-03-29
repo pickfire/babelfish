18831191e94f7e202a883f728ce6f1aa775695c0
Ivan Tham <pickfire@riseup.net>
2020-03-29 13:23:51 +0800

    chore: bump dependencies

--- src/bin/json2cbor.rs
+++ src/bin/json2cbor.rs
@@ -4,6 +4,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let reader = BufReader::new(io::stdin());
     let writer = BufWriter::new(io::stdout());
 
+    let writer = serde_cbor::ser::IoWrite::new(writer);
+
     let mut deserializer = serde_json::Deserializer::from_reader(reader);
     let mut serializer = serde_cbor::Serializer::new(writer);
 
--- src/bin/toml2cbor.rs
+++ src/bin/toml2cbor.rs
@@ -7,6 +7,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut buffer = String::new();
     reader.read_to_string(&mut buffer)?;
 
+    let writer = serde_cbor::ser::IoWrite::new(writer);
+
     let mut deserializer = serde_toml::Deserializer::new(&buffer);
     let mut serializer = serde_cbor::Serializer::new(writer);
 
